8e9d37a6703e0011070977136af44e13a5121c44
Yash Salve <bhausalve9@gmail.com>
2026-04-10 16:31:13 +0530

    fix this HAH

--- import-export-site/src/pages/Contact.jsx
+++ import-export-site/src/pages/Contact.jsx
@@ -52,7 +52,7 @@ const Contact = () => {
     setStatus({ type: "", message: "" });
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch(process.env.REACT_APP_API_URL + "/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
